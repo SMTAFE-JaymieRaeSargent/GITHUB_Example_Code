9150caaa157af44be649c0aa65d9faee96afcc4d
JayRay <jaymie-rae.sargent@smtafe.wa.edu.au>
2025-05-08 23:57:07 +0800

    ui: created basic hud
    added temp sprites and blocked out player hud

---
+++ Assets/Scripts/Stats and Leveling/UIManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Image healthBar;
+    public Image staminaBar;
+    public Image experienceBar;
+
+}
