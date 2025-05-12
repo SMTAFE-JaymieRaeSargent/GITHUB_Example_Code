0d65589c45b625a42d712def94777d924f364381
JayRay <jaymie-rae.sargent@smtafe.wa.edu.au>
2025-05-12 23:07:21 +0800

    refactor: comments and sprint
    refactored code to include comments and basic clean up. modified sprint to work only if stamina exists and to reflect stamina use and regen in ui.

--- Assets/Scripts/Player Movement/PlayerMovement.cs
+++ Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -1,92 +1,234 @@
-using UnityEngine;
+using UnityEngine; // Provides access to Unity engine features like MonoBehaviour, Transform, Input.
 
+/// <summary>
+/// Handles player movement including walking, running, crouching, jumping, and stamina regeneration.
+/// Requires a CharacterController component and should be attached to the player GameObject.
+/// </summary>
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    //the direction we are moving
+    #region Variables
+
+    // The direction the player is moving.
     [SerializeField] Vector3 _moveDirection;
-    //the reference to the CharacterController
+
+    // The reference to the CharacterController component.
     [SerializeField] CharacterController _characterController;
-    //walk, crouch, sprint, jump, gravity
+
+    // Current movement speed and movement values for walk, run, crouch, jump, and gravity.
     [SerializeField] float _movementSpeed, _walk = 5, _run = 10, _crouch = 2.5f, _jump = 8, _gravity = 20;
+
+    // Stores horizontal (x) and vertical (y) input.
     Vector2 newInput;
+
+    // Reference to the PlayerHandler for accessing player stats.
+    PlayerHandler playerHandler;
+
+    // Reference to UIManager for updating stamina bar.
+    [SerializeField] UIManager uiManager;
+
+    // Whether the player is currently moving.
+    bool isMoving;
+
+    // Whether the player is allowed to regenerate stamina.
+    bool canRegen;
+
+    // Timer for controlling regeneration delay.
+    float regenTimer;
+
+    #endregion
+    #region Unity Callbacks
+
+    /// <summary>
+    /// Called when the script instance is being loaded.
+    /// Initializes component references.
+    /// </summary>
     private void Awake()
     {
+        // Gets the CharacterController component attached to the same GameObject and stores it in _characterController.
         _characterController = GetComponent<CharacterController>();
+        // Gets the PlayerHandler component attached to the same GameObject and stores it in playerHandler.
+        playerHandler = GetComponent<PlayerHandler>();
+        // Finds the GameObject in the scene with the tag "GameManager", then gets its UIManager component and stores it in uiManager.
+        uiManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<UIManager>();
     }
 
-    void SpeedControls()
-    {
-        if (Input.GetKey(KeybindManager.keys["Sprint"]))
-        {
-            _movementSpeed = _run;
-        }
-        else if (Input.GetKey(KeybindManager.keys["Crouch"]))
-        {
-            _movementSpeed = _crouch;
-        }
-        else
-        {
-            _movementSpeed = _walk;
-        }
-    }
-    void Move()
-    {
-        //if our reference to the character controller has a value aka we ected it yay!!! woop 
-        if (_characterController != null)
-        {
-            //check of we are on the ground so we can move coz thats how people work 
-            if (_characterController.isGrounded)
-            {
-                if (Input.GetKey(KeybindManager.keys["Left"]))
-                {
-                    newInput.x = -1;
-                }
-                else if (Input.GetKey(KeybindManager.keys["Right"]))
-                {
-                    newInput.x = 1;
-                }
-                else
-                {
-                    newInput.x = 0;
-                }
-                if (Input.GetKey(KeybindManager.keys["Backward"]))
-                {
-                    newInput.y = -1;
-                }
-                else if (Input.GetKey(KeybindManager.keys["Forward"]))
-                {
-                    newInput.y = 1;
-                }
-                else
-                {
-                    newInput.y = 0;
-                }
-                // This sets _moveDirection based on player input from the Horizontal and Vertical axes.
-                _moveDirection = new Vector3(newInput.x, 0, newInput.y);
-                
-                //move in the direction we are facing
-                _moveDirection = transform.TransformDirection(_moveDirection);
-                // This multiplies the direction by the current movement speed.
-                _moveDirection *= _movementSpeed;
-                // This checks if the player has pressed the Jump button.
-                if (Input.GetKey(KeybindManager.keys["Jump"]))
-                {
-                    // This sets the y component of _moveDirection to the jump force, making the character jump.
-                    _moveDirection.y = _jump;
-                }
-            }
-            //add gravity to direction
-            _moveDirection.y -= _gravity * Time.deltaTime;
-            //apply movement
-            _characterController.Move(_moveDirection * Time.deltaTime);
-        }
-    }
-    // Update is called once per frame
+    /// <summary>
+    /// Called once per frame.
+    /// Manages timer, stamina regeneration, speed changes, and movement input.
+    /// </summary>
     void Update()
     {
+        // Updates a timer that controls when stamina regeneration is allowed again.
+        Timer();
+        // Gradually regenerates the player's stamina if allowed.
+        RegenOverTime();
+        // Adjusts the player's movement speed based on input (walk, sprint, crouch) and stamina level.
         SpeedControls();
+        // Handles player movement input and applies movement to the CharacterController.
         Move();
     }
 
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Regenerates the player's stamina over time if allowed.
+    /// </summary>
+    void RegenOverTime()
+    {
+        // Check if the player is allowed to regenerate stamina
+        if (canRegen)
+        {
+            // Check if current stamina is less than the maximum allowed
+            if (playerHandler.playerData.stamina.currentValue < playerHandler.playerData.stamina.maxValue)
+            {
+                // Increase the player's stamina over time, based on the regen rate and deltaTime
+                playerHandler.playerData.stamina.currentValue += playerHandler.playerData.stamina.value * Time.deltaTime;
+
+                // Update the stamina bar in the UI to reflect the new stamina value
+                uiManager.UpdateUI(uiManager.staminaBar,
+                    playerHandler.playerData.stamina.currentValue,
+                    playerHandler.playerData.stamina.maxValue);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Controls the timer that delays stamina regeneration after sprinting.
+    /// </summary>
+    void Timer()
+    {
+        // Check if stamina regeneration is currently disabled
+        if (!canRegen)
+        {
+            // Increment the regen timer by the time passed since the last frame
+            regenTimer += Time.deltaTime;
+            // If the regen timer has exceeded 1.5 seconds, allow stamina regeneration
+            if (regenTimer >= 1.5f)
+            {
+                // Enable stamina regeneration
+                canRegen = true;
+                // Reset the regen timer
+                regenTimer = 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Adjusts player speed based on sprint or crouch input.
+    /// Also consumes stamina while sprinting.
+    /// </summary>
+    void SpeedControls()
+    {
+        // Check if the player is pressing the Sprint key, has stamina left, and is moving
+        if (Input.GetKey(KeybindManager.keys["Sprint"]) &&
+            playerHandler.playerData.stamina.currentValue > 0 && isMoving)
+        {
+            // Set the movement speed to the sprint value
+            _movementSpeed = _run;
+            // Decrease the player's stamina by 1 per second (scaled by deltaTime)
+            playerHandler.playerData.stamina.currentValue -= 1 * Time.deltaTime;
+            // Update the stamina UI to reflect the current stamina value
+            uiManager.UpdateUI(uiManager.staminaBar,
+                playerHandler.playerData.stamina.currentValue,
+                playerHandler.playerData.stamina.maxValue);
+            // Disable stamina regeneration while sprinting
+            canRegen = false;
+            // Reset the regen timer as the player is actively sprinting
+            regenTimer = 0;
+        }
+        // Check if the player is pressing the Crouch key
+        else if (Input.GetKey(KeybindManager.keys["Crouch"]))
+        {
+            // Set the movement speed to the crouch value
+            _movementSpeed = _crouch;
+        }
+        // If no sprint or crouch keys are pressed
+        else
+        {
+            // Set the normal walk speed
+            _movementSpeed = _walk;
+        }
+    }
+
+    /// <summary>
+    /// Handles movement input, jumping, gravity, and applying it via CharacterController.
+    /// </summary>
+    void Move()
+    {
+        // Check if the CharacterController is properly assigned (not null)
+        if (_characterController != null)
+        {
+            // Check if the character is grounded (on the floor or surface)
+            if (_characterController.isGrounded)
+            {
+                // Check if the player is pressing the Left movement key
+                if (Input.GetKey(KeybindManager.keys["Left"]))
+                {
+                    // Set the horizontal input to -1 (move left)
+                    newInput.x = -1;
+                    isMoving = true;
+                }
+                // Check if the player is pressing the Right movement key
+                else if (Input.GetKey(KeybindManager.keys["Right"]))
+                {
+                    // Set the horizontal input to 1 (move right)
+                    newInput.x = 1;
+                    isMoving = true;
+                }
+                // No left or right key is pressed 
+                else
+                {
+                    // Stop horizontal movement
+                    newInput.x = 0;
+                    isMoving = false;
+                }
+
+                // Check if the player is pressing the Backward movement key
+                if (Input.GetKey(KeybindManager.keys["Backward"]))
+                {
+                    // Set the vertical input to -1 (move backward)
+                    newInput.y = -1;
+                    isMoving = true;
+                }
+                // Check if the player is pressing the Forward movement key
+                else if (Input.GetKey(KeybindManager.keys["Forward"]))
+                {
+                    // Set the vertical input to 1 (move forward)
+                    newInput.y = 1;
+                    isMoving = true;
+                }
+                // No forward or backward key is pressed
+                else
+                {
+                    // Stop vertical movement
+                    newInput.y = 0;
+                    isMoving = false;
+                }
+
+                // Apply movement direction based on the horizontal and vertical input
+                _moveDirection = new Vector3(newInput.x, 0, newInput.y);
+                // Transform movement direction relative to the player's rotation (to maintain movement direction)
+                _moveDirection = transform.TransformDirection(_moveDirection);
+                // Multiply by the movement speed to adjust the character's movement speed
+                _moveDirection *= _movementSpeed;
+
+                // Check if the player is pressing the Jump key
+                if (Input.GetKey(KeybindManager.keys["Jump"]))
+                {
+                    // Apply upward force to the movement direction (make the character jump)
+                    _moveDirection.y = _jump;
+                }
+            }
+
+            // Apply gravity to the y-axis of the movement direction
+            _moveDirection.y -= _gravity * Time.deltaTime;
+            // Move the character based on the movement direction (scaled by deltaTime for frame-independent movement)
+            _characterController.Move(_moveDirection * Time.deltaTime);
+        }
+    }
+
+    #endregion
 }
--- Assets/Scripts/Stats and Leveling/PlayerHandler.cs
+++ Assets/Scripts/Stats and Leveling/PlayerHandler.cs
@@ -1,107 +1,222 @@
-using UnityEngine;
-using System.Collections.Generic;// Allows use of generic collections like List and Dictionary.
+using UnityEngine; // Grants access to Unity’s core features like GameObjects, MonoBehaviour, Transforms, etc.
+using System.Collections.Generic; // Allows use of generic collections like List and Dictionary.
 
+/// <summary>
+/// Manages the player's health, healing, and UI updates in response to game events such as damage or collision.
+/// Attach this script to the Player GameObject.
+/// </summary>
 public class PlayerHandler : MonoBehaviour
 {
+    #region Variables
+
+    // Stores the player's statistics including health, stamina, and experience
     public PlayerStats playerData = new PlayerStats();
 
+    // Reference to the player's current spawn point in the scene
     [SerializeField] Transform spawnPoint;
+
+    // Timer used to delay healing after taking damage
     [SerializeField] float timerValue;
+
+    // Flag indicating whether the player is allowed to heal
     [SerializeField] bool canHeal = true;
+
+    // Tracks previously hit tags to avoid repeated damage from the same source
     private List<string> hitTags = new List<string>();
+
+    // Tracks currently hit tags during this frame
     private List<string> currentHitTags = new List<string>();
 
+    // Reference to the UI manager that handles UI updates
     public UIManager uiManager;
 
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Applies damage to the player and updates the health UI. Disables healing temporarily.
+    /// </summary>
+    /// <param name="damageValue">The amount of damage to apply to the player.</param>
     public void DamagePlayer(float damageValue)
     {
+        // Reset healing timer and disable healing
         timerValue = 0;
         canHeal = false;
+
+        // Subtract damage from the player's current health
         playerData.health.currentValue -= damageValue;
+
+        // Update the health bar in the UI
         uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
     }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Gradually heals the player over time if healing is allowed and the player's health is not full.
+    /// </summary>
     void HealOverTime()
     {
+        // Check if the player is allowed to regenerate health
         if (canHeal)
         {
+            // Check if current health is less than the maximum allowed and the player is alive
+
             if (playerData.health.currentValue < playerData.health.maxValue && playerData.health.currentValue > 0)
             {
-                //current health to increase by a value over time 
+                // Increase health over time based on the health regen value
                 playerData.health.currentValue += playerData.health.value * Time.deltaTime;
+
+                // Update the health bar UI to reflect new health
                 uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
             }
         }
     }
+
+    /// <summary>
+    /// Tracks the time since the player last took damage and re-enables healing after a delay.
+    /// </summary>
     void Timer()
     {
+        // Check if health regeneration is currently disabled
         if (!canHeal)
         {
+            // Increment the regen timer by the time passed since the last frame
             timerValue += Time.deltaTime;
+
+            // If the regen timer has exceeded 1.5 seconds, allow health regeneration
             if (timerValue >= 1.5f)
             {
-                //allow healing
+                // Enable health regeneration
                 canHeal = true;
-                //reset timer
+                // Reset the regen timer
                 timerValue = 0;
             }
         }
     }
+
+    #endregion
+
+    #region Unity Callbacks
+
+    /// <summary>
+    /// Unity's Start method. Initializes the UI with the player's starting stats.
+    /// </summary>
     private void Start()
     {
+        // Find and assign the UI Manager from the GameManager
+        uiManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<UIManager>();
 
+        // Initialize the health, stamina, and experience bars in the UI
         uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
         uiManager.UpdateUI(uiManager.staminaBar, playerData.stamina.currentValue, playerData.stamina.maxValue);
         uiManager.UpdateUI(uiManager.experienceBar, playerData.experience.currentValue, playerData.experience.maxValue);
-
     }
+
+    /// <summary>
+    /// Called when the player enters a trigger collider.
+    /// Used to temporarily boost healing or update the spawn point.
+    /// </summary>
+    /// <param name="other">The collider the player has entered.</param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Heal")
         {
+            // Double the healing rate while inside the Heal trigger
             playerData.health.value *= 2;
         }
-
     }
+
+    /// <summary>
+    /// Called when the player exits a trigger collider.
+    /// Resets healing and updates spawn points.
+    /// </summary>
+    /// <param name="other">The collider the player has exited.</param>
     private void OnTriggerExit(Collider other)
     {
+        // Check if the other object the player collided with has the tag "Heal"
         if (other.tag == "Heal")
         {
+            // Reset the healing rate when leaving the Heal trigger
             playerData.health.value /= 2;
         }
+        // Check if the other object the player collided with has the tag "SpawnPoint"
         if (other.tag == "SpawnPoint")
         {
+            // Update the player's spawn point to the exited collider's transform
             spawnPoint = other.transform;
         }
     }
 
+    /// <summary>
+    /// Called when the player collides with another object using a CharacterController.
+    /// Applies damage on contact with damage-dealing objects.
+    /// </summary>
+    /// <param name="hit">The collider information from the collision.</param>
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
+        // Store the current tag of the object being hit in the currentHitTags list
         currentHitTags.Add(hit.gameObject.tag);
-
+        // Check if the object being hit has the tag "Damage" and if it hasn't already been processed
         if (hit.gameObject.CompareTag("Damage") && !hitTags.Contains(hit.gameObject.tag))
         {
+            // Log a message indicating that damage was hit
             Debug.Log("Hit damage");
+
+            // Apply damage to the player by calling the DamagePlayer method with a damage value of 10
             DamagePlayer(10);
+            // Add the "Damage" tag to the hitTags list to prevent repeated damage from the same object
             hitTags.Add(hit.gameObject.tag);
         }
     }
+
+    /// <summary>
+    /// Unity's LateUpdate method. Clears old collision tags to allow damage again on the next frame.
+    /// </summary>
     private void LateUpdate()
     {
+        // Initialize a variable to track if any current hit tags match the processed ones
         int i = 0;
+
+        // Check if the currentHitTags list has any tags stored (i.e., check if there were any hits in the current frame)
         if (currentHitTags.Count > 0)
         {
+            // Iterate through each tag in the currentHitTags list
             foreach (string tag in currentHitTags)
             {
+                // If the tag is already present in the hitTags list (i.e., the tag was already processed for damage)
                 if (hitTags.Contains(tag))
                 {
+                    // Set 'i' to 1 to indicate that a matching tag was found
                     i = 1;
                 }
             }
         }
+
+        // If no matching tags were found (i == 0)
         if (i == 0)
         {
+            //  Clear the hitTags list to allow for future damage
             hitTags.Clear();
         }
+
+        // Clear the currentHitTags list for the next frame to ensure fresh processing
         currentHitTags.Clear();
     }
+
+    /// <summary>
+    /// Unity's Update method. Handles healing over time and damage cooldown logic.
+    /// </summary>
+    private void Update()
+    {
+        // Call the HealOverTime method to potentially heal the player over time based on certain conditions
+        HealOverTime();
+        // Call the Timer method to update the regeneration timer and check if regeneration is allowed
+        Timer();
+    }
+
+    #endregion
 }
