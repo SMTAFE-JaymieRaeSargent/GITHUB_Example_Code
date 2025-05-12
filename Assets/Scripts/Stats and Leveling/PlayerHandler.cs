763f50bb94abf1e57c864a011ec80bf342ec0b1b
JayRay <jaymie-rae.sargent@smtafe.wa.edu.au>
2025-05-12 18:59:52 +0800

    modification: health and damage
    Created functions for health and damage. needs player movement to be able to test behavior

--- Assets/Scripts/Stats and Leveling/PlayerHandler.cs
+++ Assets/Scripts/Stats and Leveling/PlayerHandler.cs
@@ -3,15 +3,79 @@ using UnityEngine;
 public class PlayerHandler : MonoBehaviour
 {
     public PlayerStats playerData = new PlayerStats();
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] float timerValue;
+    [SerializeField] bool canHeal = true;
+
+    public UIManager uiManager;
+
+    public void DamagePlayer(float damageValue)
     {
+        timerValue = 0;
+        canHeal = false;
+        playerData.health.currentValue -= damageValue;
+        uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
+    }
+    void HealOverTime()
+    {
+        if (canHeal)
+        {
+            if (playerData.health.currentValue < playerData.health.maxValue && playerData.health.currentValue > 0)
+            {
+                //current health to increase by a value over time 
+                playerData.health.currentValue += playerData.health.value * Time.deltaTime;
+                uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
+            }
+        }
+    }
+    void Timer()
+    {
+        if (!canHeal)
+        {
+            timerValue += Time.deltaTime;
+            if (timerValue >= 1.5f)
+            {
+                //allow healing
+                canHeal = true;
+                //reset timer
+                timerValue = 0;
+            }
+        }
+    }
+    private void Start()
+    {
+       
+        uiManager.UpdateUI(uiManager.healthBar, playerData.health.currentValue, playerData.health.maxValue);
+        uiManager.UpdateUI(uiManager.staminaBar, playerData.stamina.currentValue, playerData.stamina.maxValue);
+        uiManager.UpdateUI(uiManager.experienceBar, playerData.experience.currentValue, playerData.experience.maxValue);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Heal")
+        {
+            playerData.health.value *= 2;
+        }
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.tag == "Heal")
+        {
+            playerData.health.value /= 2;
+        }
+        if (other.tag == "SpawnPoint")
+        {
+            spawnPoint = other.transform;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+            DamagePlayer(10);
+        }
     }
 }
--- Assets/Scripts/Stats and Leveling/UIManager.cs
+++ Assets/Scripts/Stats and Leveling/UIManager.cs
@@ -7,4 +7,8 @@ public class UIManager : MonoBehaviour
     public Image staminaBar;
     public Image experienceBar;
 
+    public void UpdateUI(Image displayImage, float currentValue, float maxValue)
+    {
+        displayImage.fillAmount = Mathf.Clamp01(currentValue / maxValue);
+    }
 }
