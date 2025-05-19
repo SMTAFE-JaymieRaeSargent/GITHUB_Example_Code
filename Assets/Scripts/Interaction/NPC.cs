1f57c4081f1f90cbc0a9b3aeda32101c28744739
JayRay <jaymie-rae.sargent@smtafe.wa.edu.au>
2025-05-19 23:18:27 +0800

    modification
    Dialogue added to NPC
    DialogueManager Script created
    UI Set up.
    
    issue with freezing player and rotation till end of conversation

--- Assets/Scripts/Interaction/NPC.cs
+++ Assets/Scripts/Interaction/NPC.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class NPC : MonoBehaviour, IInteractable
 {
     private string toolTip = "";
-
+    [SerializeField] string[] _lines;
+    [SerializeField] string _name;
+    [SerializeField] Sprite _face;
     private void Start()
     {
         toolTip = $"Press {KeybindManager.keys["Interact"].ToString()} to talk";
     }
     public void OnInteraction()
     {
-        Debug.Log("Talk to NPC");
+       // Debug.Log("Talk to NPC");
+        //Run behaviour from Dialogue Manager instance 
+        DialogueManager.instance.OnActive(_lines, _name, _face);
     }
 
     public string ToolTip()
